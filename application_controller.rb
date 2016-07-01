fa3a6ffae1c2ce614447b9b7b7a987d54e7d0cbd
Jeff Katz <jeffreyerickatz@gmail.com>
2016-07-01 10:05:39 -0400

    commit

---
+++ application_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < Sinatra::Base
+  # new
+
+  # create
+
+  # show
+
+  # index
+
+  # edit
+
+  # update
+  
+end
+
+# Write in all of the restful actions (except for destroy).
+
+# You should write the code exactly as you would in 
+# a normal sinatra codebase.  This means act on the params as you normally would
+# render and redirect as you normally would in a sinatra application.
+
