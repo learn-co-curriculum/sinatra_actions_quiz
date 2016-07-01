f4359a9bea262f2ccdcb6233950f9dfd5a0f376e
Jeff Katz <jeffreyerickatz@gmail.com>
2016-07-01 10:40:13 -0400

    updating quiz

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,5 @@
+# Write your name here: 
+
 class UsersController < Sinatra::Base
   # new
 
@@ -13,9 +15,10 @@ class UsersController < Sinatra::Base
   
 end
 
+# Fork this code make a pull request
+
 # Write in all of the restful actions (except for destroy).
 
 # You should write the code exactly as you would in 
 # a normal sinatra codebase.  This means act on the params as you normally would
 # render and redirect as you normally would in a sinatra application.
-
