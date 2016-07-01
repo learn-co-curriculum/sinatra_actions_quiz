a7c0cb8badf3cb068260c293128585d6fe976f62
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-01 11:29:33 -0400

    Done.

--- application_controller.rb
+++ application_controller.rb
@@ -2,16 +2,42 @@
 
 class UsersController < Sinatra::Base
   # new
+  get '/users/new' do 
+    erb :new
+  end
 
   # create
+  post '/users' do 
+    @user = User.create(params)
+    redirect to "/users/#{@user.id}"
+  end
 
   # show
+  get '/user/:id' do 
+    @user = User.find_by_id(params[:id])
+    erb :show
+  end
 
   # index
+  get '/users' do 
+    @users = User.all
+    erb :index
+  end
 
   # edit
+  get '/users/:id/edit' do 
+    @user = User.find_by_id(params[:id])
+    erb :edit
+  end
 
   # update
+  patch '/users/:id' do
+    @user = User.find_by_id(params[:id])
+    @user.name = params[:name]
+    # updates other attributes like above
+    @user.save
+    redirect to "/users/#{@user.id}"
+  end
   
 end
 
