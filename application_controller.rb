820d6b745bc54cc8f91c6a9cfa4ee0badad5630f
greatermeans <gakouri@gmail.com>
2016-07-01 10:53:31 -0400

    finished

--- application_controller.rb
+++ application_controller.rb
@@ -1,17 +1,44 @@
-# Write your name here: 
+# George A
 
 class UsersController < Sinatra::Base
   # new
-
+  get '/users/new' do 
+  	erb :new
+  end
   # create
+  post '/users' do 
+  	@user = User.create(params["user"])
+  	redirect "/users/#{@user.id}"
+  end
 
   # show
 
+  get '/users/:id'
+  	@user = User.find(params["id"])
+  	erb :show
+  end
+
   # index
+  get '/users'
+  	@users = User.all
+  	erb :index
+  end
 
   # edit
+  get '/users/:id/edit' do 
+  	@user = User.find(params["id"])
+  	erb :edit
+  end
+
 
   # update
+  patch '/users' do 
+  	@user = User.find(params["id"])
+  	@user.update(params["user"])
+  	# if user has optional args
+  	@user.hobbies << Hobby.find_or_create_by(params["hobbies"]) if !params["hobbies"]["name"].empty?
+  	redirect "/users/#{@user.id}"
+  end
   
 end
 
