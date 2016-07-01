967a94a55ce45833d93eb4bbf11935657192a6de
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-01 13:12:36 -0400

    quiz done

--- application_controller.rb
+++ application_controller.rb
@@ -1,17 +1,46 @@
 # Write your name here: 
+#assuming the object is owners
 
 class UsersController < Sinatra::Base
   # new
+  get '/owners/new' do
+    erb :new #a new erb will have a form where the user submits information on the object
+  end
 
   # create
+  post '/owners' do
+    @owner = Owner.create(params)
+    redirect to '/owners/#{params[:id]}'
+  end
+
 
   # show
+  get '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    erb :show #a new show erb will print the attributes of the object
+  end
 
   # index
+  get '/owners' do
+    @owner = Owners.all
+    erb :index
+  end
 
   # edit
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])
+    erb :edit
+  end
 
-  # update
+  #update
+  patch 'owners/:id' do #remember to do the method override on post
+    @owner = Owner.find(params[:id])
+    @owner.name = params[:name]
+    @owner.pets = params[:pets]
+    @owner.save
+    erb :show
+  end
+
   
 end
 
