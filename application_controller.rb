9a3632b4f79843fed53658169cb555720552304e
kevin hernon <kevinhernon@kevins-MacBook-Air.local>
2016-07-01 10:57:09 -0400

    sinatra quiz

--- application_controller.rb
+++ application_controller.rb
@@ -2,16 +2,40 @@
 
 class UsersController < Sinatra::Base
   # new
+  get '/resources/new' do
+    erb :'new.html'
+  end
 
   # create
+  post '/resources/new' do
+    resource = Resource.create(params[:resource])
+    redirect '/resources'
+  end
 
   # show
+  get '/resources/:id' do
+    resource = Resource.find(params[:id])
+    erb :'show.html'
+  end
 
   # index
+  get '/resources' do
+    @resources = Resource.all
+    erb :'index.html'
+  end
 
   # edit
+  get '/resources/:id/edit' do
+    @resource = Resource.find(params[:id])
+    erb :'edit.html'
+  end
 
   # update
+  patch '/resources/:id/edit' do
+    resource = Resource.find(params[:id])
+    resource.update(name: params[:name], etc etc)
+    redirect "resources/#{resource.id}"
+  end
   
 end
 
